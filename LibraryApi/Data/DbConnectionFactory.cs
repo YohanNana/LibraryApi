cd5a748c85991e3ff187742ff654810de1418608
Yohan Nanayakkara <yohan.nana16@gmail.com>
2026-03-03 23:53:46 +0530

    Repos added

--- LibraryApi/Data/DbConnectionFactory.cs
+++ LibraryApi/Data/DbConnectionFactory.cs
@@ -1,6 +1,22 @@
-﻿namespace LibraryApi.Data
+﻿using Microsoft.Data.SqlClient;
+
+namespace LibraryApi.Data
 {
     public class DbConnectionFactory
     {
+        private readonly string _connectionString;
+
+        public DbConnectionFactory(IConfiguration config)
+        {
+            _connectionString = config.GetConnectionString("DefaultConnection")
+                ?? throw new InvalidOperationException("Connection string not found.");
+        }
+
+        public SqlConnection Create()
+        {
+            var conn = new SqlConnection(_connectionString);
+            conn.Open();
+            return conn;
+        }
     }
 }
--- LibraryApi/Repositories/IBookRepository.cs
+++ LibraryApi/Repositories/IBookRepository.cs
@@ -1,6 +1,13 @@
-﻿namespace LibraryApi.Repositories
+﻿using LibraryApi.Models;
+
+namespace LibraryApi.Repositories
 {
-    public class IBookRepository
+    public interface IBookRepository
     {
+        Task<IEnumerable<Book>> GetAllAsync();
+        Task<Book?> GetByOpenLibraryIdAsync(string openLibraryId);
+        Task<Book> InsertAsync(Book book);
+        Task<bool> UpdateNotesAsync(string openLibraryId, string? notes);
+        Task<bool> DeleteAsync(string openLibraryId);
     }
 }
