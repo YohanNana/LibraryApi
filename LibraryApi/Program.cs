18d36ff3dea0fe85dbf772db5bfbf0bb3fcfe0ec
Yohan Nanayakkara <yohan.nana16@gmail.com>
2026-03-04 00:52:44 +0530

    json and program file

--- LibraryApi/Program.cs
+++ LibraryApi/Program.cs
@@ -1,3 +1,7 @@
+using LibraryApi.Data;
+using LibraryApi.Repositories;
+using LibraryApi.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,17 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Register services
+builder.Services.AddSingleton<DbConnectionFactory>();
+builder.Services.AddScoped<IBookRepository, BookRepository>();
+builder.Services.AddHttpClient<BookService>();
+builder.Services.AddScoped<IBookService, BookService>(sp =>
+    new BookService(
+        sp.GetRequiredService<IBookRepository>(),
+        sp.GetRequiredService<IHttpClientFactory>().CreateClient(nameof(BookService)),
+        sp.GetRequiredService<ILogger<BookService>>()
+    ));
 
 var app = builder.Build();
 
